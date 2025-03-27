7271dcb3c74d5b6a38fb418d05ecaab28b4574fa
aaliyahEvanie <bernice.botes42@gmail.com>
2025-03-27 17:46:26 +0100

    base environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    development: true,
+    bikeUrl:  'https://bikeindex.org:443/api/v3', 
+    googleApiKey: 'AIzaSyD86kzbC9k_O6Pvo2mWK0x1cGeWb4m6jvw',
+    googleApiUrl: 'https://maps.googleapis.com/maps/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    bikeUrl: '', 
+    googleApiKey: '',
+    googleApiUrl:''
+};
